d69dc99b34ae8fdada18b5c419cbdff357442338
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2016-01-26 20:14:13 +1100

    misc

--- postproc.sh
+++ postproc.sh
@@ -2,16 +2,46 @@
 
 cd module
 
-string=     "  String searchQuery = \"SELECT uuid, response \"+
-                       \"  FROM latestNonDeletedArchEntFormattedIdentifiers"
-replacement="  String searchQuery = \"SELECT uuid, text \"+
-                       \"  FROM localRecords"
-perl -0777 -i.original -pe 's/$string/$replacement/igs' ui_logic.bsh
+# Make local records work with the search feature
+string="String searchQuery = \"SELECT uuid, response \"+
+                       \"  FROM latestNonDeletedArchEntFormattedIdentifiers  \"+
+                       \" WHERE uuid in (SELECT uuid \"+
+                       \"                  FROM latestNonDeletedArchEntIdentifiers \"+
+                       \"                 WHERE measure LIKE '\"+term+\"'||'%'  \"+
+                       \"                   AND ( aenttypename LIKE '\"+type+\"' OR '' = '\"+type+\"' ) \"+
+                       \"                )  \"+
+                       \" ORDER BY response \"+
+                       \" LIMIT ? \"+
+                       \"OFFSET ? \";"
+replacement="String searchQuery = \"SELECT uuid, response \"+
+                       \"  FROM localRecords \"+
+                       \"ORDER BY response \"+
+                       \" LIMIT ? \"+
+                       \"OFFSET ? \";"
+perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_logic.bsh
 
-
-string=     "  saveTabGroup(\"Field\");"
+# Make local records work with autosaving
+string="  saveTabGroup(\"Field\");"
 replacement="  saveTabGroup(\"Field\", \"saveFieldCallback()\");"
-perl -0777 -i.original -pe 's/$string/$replacement/igs' ui_logic.bsh
+perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_logic.bsh
+
+# Make local records work with duplication
+string="onSave(autosaveUuid, autosaveNewRecord) {
+          setUuid(tabgroup, autosaveUuid);
+        }"
+replacement="onSave(autosaveUuid, autosaveNewRecord) {
+          setUuid(tabgroup, autosaveUuid);
+          saveFieldCallback();
+        }"
+perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_logic.bsh
+
+# Make local records work with deletion
+string="reallyDeleteField(){
+  String tabgroup = \"Field\";"
+replacement="reallyDeleteField(){
+  String tabgroup = \"Field\";
+  deleteFieldCallback();"
+perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_logic.bsh
 
 
-rm ui_logic.bsh.original
+#rm ui_logic.bsh.original
