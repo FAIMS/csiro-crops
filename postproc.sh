643105fc75c4758ff4c2c9ca7d182034246d9548
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2016-01-29 12:59:04 +1100

    Everything seems to be working except for geometry saving. Damn thing.

--- postproc.sh
+++ postproc.sh
@@ -2,6 +2,19 @@
 
 cd module
 
+# Disallow users from logging in with a blank dropdown
+string="onClickLoginLogin () {"
+replacement="onClickLoginLogin () {
+  String val = getDropdownItemValue();
+  if (isNull(val)) {
+    String msgTitle = \"Cannot log in\";
+    String msgBody  = \"A user must be selected in order to log in.\";
+    showWarning(msgTitle, msgBody);
+    return;
+  }
+"
+perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_logic.bsh
+
 # Make local records work with the search feature
 string="String searchQuery = \"SELECT uuid, response \"+
                        \"  FROM latestNonDeletedArchEntFormattedIdentifiers  \"+
@@ -13,11 +26,12 @@ string="String searchQuery = \"SELECT uuid, response \"+
                        \" ORDER BY response \"+
                        \" LIMIT ? \"+
                        \"OFFSET ? \";"
-replacement="String searchQuery = \"SELECT uuid, response \"+
-                       \"  FROM localRecords \"+
-                       \"ORDER BY response \"+
-                       \" LIMIT ? \"+
-                       \"OFFSET ? \";"
+replacement="String searchQuery = \"  SELECT uuid, response \"+
+                       \"    FROM localRecord \"+
+                       \"   WHERE response LIKE '\"+term+\"'||'%'  \"+
+                       \"ORDER BY uuid DESC \"+
+                       \"   LIMIT ? \"+
+                       \"  OFFSET ? \";"
 perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_logic.bsh
 
 # Make local records work with autosaving
@@ -44,4 +58,4 @@ replacement="reallyDeleteField(){
 perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_logic.bsh
 
 
-#rm ui_logic.bsh.original
+rm ui_logic.bsh.original
